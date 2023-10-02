caddf3ab70db427263f4d1be82be4d498891fd32
Marko Silla <marko.silla@ericsson.com>
2023-10-03 01:28:28 +0300

    First version with DFS

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mindworks
+Loo C#-is klass, mis arvutab tipupaaridena etteantud etteantud atsüklilises suunatud graafis kõik võimalikud teed kahe tipu vahel. 
+Tippe tähistavad juhuslikud täisarvud. Graaf on esitatud kujul List<Tuple<int, int>> kus iga Tuple<int, int> esitab graafil seost 
+esimeselt tuple elemendilt teisele. Kõik võimalikud teed tuleb tagastada List<List<int>> struktuuris.
+
+Näiteks graafi
+
+var graph = new List<(int, int)> { (1, 2), (1, 3), (2, 4), (3, 4) };
+
+Korral küsides seoseid tippude 1 ja 4 vahel peab programm tagastama
+
+[[1,2,4], [1,3,4]]
+
+Vastus: Pidasin silmas koodi loetavust ja lihtsust ning üritasin vältida asjatut keerukust.
+Algoritmi valisin samuti lihtsust silmas pidades: https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
---
+++ graph/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var graph = new List<(int, int)> { (1, 2), (1, 3), (2, 4), (3, 4) };
+
+var ret = FindPaths(graph, 1, 4);
+
+ret.ForEach(x => { x.ForEach(y => Console.Write($"{y} ")); Console.WriteLine(); });
+
+static List<List<int>> FindPaths(List<(int, int)> graph, int src, int dest) {
+    var paths = new List<List<int>>();
+
+    DFS(graph, src, dest, new List<int>(), paths);
+
+    return paths;
+}
+
+static void DFS(List<(int, int)> graph, int current, int dest, List<int> currentPath, List<List<int>> paths)
+{
+    currentPath = new List<int>(currentPath) { current };
+
+    if (current == dest)
+    {
+        paths.Add(currentPath);
+        return;
+    }
+
+    foreach (var edge in graph.Where(x => current == x.Item1))
+    {
+        if (currentPath.Contains(edge.Item2))
+            continue; /* Already visited */
+
+        DFS(graph, edge.Item2, dest, currentPath, paths);
+    }
+}
