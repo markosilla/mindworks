b9b82426cb42cfd56cdc1cb34002769fd4098056
Marko Silla <marko.silla@ericsson.com>
2023-10-03 10:01:12 +0300

    More testing and simplification

--- graph/Program.cs
+++ graph/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var graph = new List<(int, int)> { (1, 2), (1, 3), (2, 4), (3, 4) };
+/* var graph = new List<(int, int)> { (3, 4), (1, 2), (1, 3), (2, 4) };
+var ret = FindPaths(graph, 1, 4); */
 
-var ret = FindPaths(graph, 1, 4);
+/* var graph = new List<(int, int)> { ('a', 'b'), ('a', 'c'), ('a', 'd'), ('a', 'e'), ('c', 'e'), ('c', 'd'), ('b', 'd'), ('d', 'e'), ('a', 'x') };
+var ret = FindPaths(graph, 'a', 'e');
+ret.ForEach(x => { x.ForEach(y => Console.Write($"{(char)y} ")); Console.WriteLine(); }); */
 
+var graph = new List<(int, int)> { (5, 11), (2, 11), (11, 10), (11, 9), (11, 7), (7, 8), (9, 8), (10, 3), (3, 8), /*(3,3)*/ };
+var ret = FindPaths(graph, 5, 8);
 ret.ForEach(x => { x.ForEach(y => Console.Write($"{y} ")); Console.WriteLine(); });
 
 static List<List<int>> FindPaths(List<(int, int)> graph, int src, int dest) {
@@ -26,9 +31,8 @@ static void DFS(List<(int, int)> graph, int current, int dest, List<int> current
 
     foreach (var edge in graph.Where(x => current == x.Item1))
     {
-        if (currentPath.Contains(edge.Item2))
-            continue; /* Already visited */
-
         DFS(graph, edge.Item2, dest, currentPath, paths);
     }
 }
+
+
