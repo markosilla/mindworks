dba656596af01e1fb4867df64a6920a6c3b67ddc
Marko Silla <marko.silla@ericsson.com>
2023-10-06 09:37:05 +0300

    Add links to images
    * Corrected directions for graph3
    * Added more testing

--- graph/Program.cs
+++ graph/Program.cs
@@ -1,14 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/* var graph = new List<(int, int)> { (3, 4), (1, 2), (1, 3), (2, 4) };
-var ret = FindPaths(graph, 1, 4); */
+var graph1 = new List<(int, int)> { (3, 4), (1, 2), (1, 3), (2, 4) };
+var ret = FindPaths(graph1, 1, 4);
+ret.ForEach(x => { x.ForEach(y => Console.Write($"{y} ")); Console.WriteLine(); });
 
-/* var graph = new List<(int, int)> { ('a', 'b'), ('a', 'c'), ('a', 'd'), ('a', 'e'), ('c', 'e'), ('c', 'd'), ('b', 'd'), ('d', 'e'), ('a', 'x') };
-var ret = FindPaths(graph, 'a', 'e');
-ret.ForEach(x => { x.ForEach(y => Console.Write($"{(char)y} ")); Console.WriteLine(); }); */
+/* https://en.wikipedia.org/wiki/Directed_acyclic_graph#/media/File:Tred-G.svg */
+var graph2 = new List<(int, int)> { ('a', 'b'), ('a', 'c'), ('a', 'd'), ('a', 'e'), ('c', 'e'), ('c', 'd'), ('b', 'd'), ('d', 'e'), ('a', 'x') };
+ret = FindPaths(graph2, 'a', 'e');
+ret.ForEach(x => { x.ForEach(y => Console.Write($"{(char)y} ")); Console.WriteLine(); });
 
-var graph = new List<(int, int)> { (5, 11), (2, 11), (11, 10), (11, 9), (11, 7), (7, 8), (9, 8), (10, 3), (3, 8), /*(3,3)*/ };
-var ret = FindPaths(graph, 5, 8);
+/* https://qph.cf2.quoracdn.net/main-qimg-0a231e2af88a88c5244e7bb5201aec43 */
+var graph3 = new List<(int, int)> { (5, 11), (11, 2), (11, 10), (11, 9), (7, 11), (7, 8), (8, 9), (3, 10), (3, 8) };
+ret = FindPaths(graph3, 5, 9);
+/* ret = FindPaths(graph3, 7, 9); */
 ret.ForEach(x => { x.ForEach(y => Console.Write($"{y} ")); Console.WriteLine(); });
 
 static List<List<int>> FindPaths(List<(int, int)> graph, int src, int dest) {
